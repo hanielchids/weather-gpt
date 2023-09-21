f27019f1d3b566d823929ea32e552df40bb05a20
hanielchids <hanielchids@gmail.com>
2023-09-21 12:58:09 +0200

    container fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Card, Divider, Subtitle, Text } from "@tremor/react";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#394F68] to-[#183B7B] p-10 flex flex-col justifyy-center items-center">
-      <Card className="max-w-4xl mx-auto">
+      <div className="p-10 bg-white container max-w-4xl mx-auto rounded shadow-lg">
         <Text className="text-xl font-bold text-center mb-10">
           GPT Weather AI
         </Text>
@@ -15,10 +15,10 @@ export default function Home() {
 
         <Divider className="my-10" />
 
-        <Card className="bg-gradient-to-br from-[#394F68] to-[#183B7B]">
+        <div className="container p-10 bg-gradient-to-br from-[#394F68] to-[#183B7B]">
           <CityPicker />
-        </Card>
-      </Card>
+        </div>
+      </div>
     </div>
   );
 }
